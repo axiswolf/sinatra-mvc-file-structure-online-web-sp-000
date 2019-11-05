1b8041d97ae0ba38e955fd4fb7fdb5461056f2fc
Karen V <karenvo@usc.edu>
2019-11-05 01:38:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,20 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+
+end
+
+class Dog
+  attr_accessor :name, :breed, :age
+  dog = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    dog
+  end
+
+  def all
+    self.count
+  end
 end
