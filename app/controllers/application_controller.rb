c7db9536f8b9ec4a2ab5dc33778c3a90078bcbec
Karen V <karenvo@usc.edu>
2019-11-05 01:41:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,13 @@ end
 
 class Dog
   attr_accessor :name, :breed, :age
+  dogs = []
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+  end
+  def self.all
+    
   end
 end
